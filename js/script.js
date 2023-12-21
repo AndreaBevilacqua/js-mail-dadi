6a4f12fd5f95eb0abd7f9046d10024106797ee82
AndreaBevilacqua <andrebevi2000@gmail.com>
2023-12-21 18:40:55 +0100

    added list todo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('JS OK')
+
+// ESERCIZIO 1 
+
+/* 
+ 1 - Recupero elementi dal DOM
+ 2 - Creo una variabile per il primo giocatore che generi un numero random da 1 a 6
+ 3 - Creo una variabile per il secondo giocatore che generi un numero random da 1 a 6
+ 4 - Stabilisco chi é il vincitore 
+ 5 - Stampo in pagina il vincitore e i risulati 
+*/
